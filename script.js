9614d3c811b4ec750ddb8ebdf2c763e9d55a2fe2
grandmasai <31676205+grandmasai@users.noreply.github.com>
2017-09-07 17:43:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log('hello world!')
-document.write('<h1> Hi!!!!</h1>')
+adj_List=["smelly","silly","ugly"];
+let sentence1="My dog is "+adj_List[Math.floor(Math.random()*adj_List.length)]+", "+"but I love him."
+let sentence2="He is "+adj_List[Math.floor(Math.random()*adj_List.length)]+", "+"but he is confident and successful."
+let sentence3="No matter how "+adj_List[Math.floor(Math.random()*adj_List.length)]+" he is, we should respect him."
+let word=prompt("Please enter an negative adjective")
+let sentence4="My dog is "+word+", "+"but I love him."
+document.write(sentence1+sentence2+sentence3+sentence4)
